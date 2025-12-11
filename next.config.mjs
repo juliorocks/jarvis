a8c8c67019bee885b4d10f1fe81d92fde30d1039
juliorocks <30703281+juliorocks@users.noreply.github.com>
2025-12-11 18:39:11 -0300

    Configure Cloudflare Adapter for Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // Enable source maps for easier debugging
+    productionBrowserSourceMaps: true,
     experimental: {
         workerThreads: false,
         cpus: 1
     }
 };
+
+if (process.env.NODE_ENV === 'development') {
+    (async () => {
+        await setupDevPlatform();
+    })();
+}
 
 export default nextConfig;
